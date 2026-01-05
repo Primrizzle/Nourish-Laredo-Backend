19510d481d3b262c4a02e15a00a2155e5a5f3842
primrizzle <ashprimrose94@gmail.com>
2026-01-04 23:55:23 -0600

    Created Event model and connected to front end, set up events table to CMS

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Event
+from .serializers import EventSerializer
+
+@api_view(["GET"])
+def event_list(request):
+    events = Event.objects.all().order_by("-date")
+    serializer = EventSerializer(events, many=True)
+    return Response(serializer.data)
